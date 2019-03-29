607c1b3d583112852f05bbc74f8d7d868bb59f28
Heorhii <grishasht@bigmir.net>
2019-03-29 20:55:13 +0200

    Add homework

---
+++ com/Main.java
@@ -0,0 +1,10 @@
+package com;
+
+import org.junit.Assert;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("\n\nShtefanovych Heorhii\n\n");
+        Assert.fail("Fail");
+    }
+}
